669c3803eb163cf70554fb1b607d2ad9e251be3e
rick <technoweenie@gmail.com>
2011-02-13 12:11:01 -0800

    allow custom IDs

--- lib/gravastar/cluster.rb
+++ lib/gravastar/cluster.rb
@@ -1,14 +1,22 @@
 module Gravastar
   class Cluster
-    include Toy::Store
+    include Toy::Store, ActiveModel::Callbacks
     store :redis, Redis.new(:db => 1)
 
     attribute :name,  String
     attribute :email, String
 
-    def search(query=nil, options = {})
+    before_create :scrub_attributes
+
+    def search(query={}, options = {})
       query['stars.cluster_id'] = id
       Gravastar::Star.search(query, options)
+    end
+
+  private
+    def scrub_attributes
+      self.name ||= id
+      self.id     = id.to_s.parameterize
     end
   end
 end
