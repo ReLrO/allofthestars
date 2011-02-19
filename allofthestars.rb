8783eaa69272e17a6279dfa9d25217036a56bd0c
rick <technoweenie@gmail.com>
2011-02-19 10:58:45 -0800

    read optional config file

--- allofthestars.rb
+++ allofthestars.rb
@@ -5,16 +5,20 @@ module AllOfTheStars
   class << self
     attr_accessor :env, :riak_client
   end
-  self.env = (ENV['RACK_ENV'] || ENV['STARS_ENV'] || :dev).to_sym
 end
 
-Bundler.require(:default, AllOfTheStars.env)
+Bundler.require(:default)
 $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 
 options = {:http_backend => :Excon}
-if AllOfTheStars.env == :production
-  options[:host] = 'riak1.allofthestars.com'
+config  = File.expand_path('../db/config.json', __FILE__)
+if File.exist?(config)
+  data = ActiveSupport::JSON.decode(IO.read(config))
+  data.each do |key, value|
+    options[key.to_sym] = value
+  end
 end
+
 AllOfTheStars.riak_client = Riak::Client.new(options)
 require 'allofthestars/cluster'
 require 'allofthestars/star'
